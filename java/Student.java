feb070c51cda5401cb6649bb35f61342653b38c6
Anjanaparu <krishnaanjana253@gmail.com>
2026-01-29 12:13:02 +0530

    commit

---
+++ java/Student.java
@@ -0,0 +1,16 @@
+public class Student{
+    void msg()
+    {
+        System.out.println("Hi student");
+    }}
+    class StudentDemo
+    {
+        public static void main(String args[])
+{
+Student ob=new Student();
+ob.msg();
+    
+        
+    }
+    
+}
